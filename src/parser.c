ade7a2b7ee1ce5ab11ac1d20824c75802dc91405
Johan Van den Brande <johan@vandenbrande.com>
2020-11-09 17:21:02 +0100

    Fix buffer overrun on next with name

--- src/parser.c
+++ src/parser.c
@@ -1194,7 +1194,7 @@ do_next(basic_type* rv)
     accept(T_VARIABLE_NUMBER);
     if ( strcmp(var_name, f->variable_name) != 0 )
     {
-      char _error[32];
+      char _error[40];
       snprintf(_error, sizeof(_error), "EXPECTED NEXT WITH %s, GOT %s", var_name, f->variable_name);
       error(_error);
       return 0;
